65c6b72b66501500367380bd66db01f40cbc83b5
William Maphanga <williesdk@gmail.com>
2022-07-30 21:51:43 +0200

    added root index endpoint

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import HTMLResponse
+from fastapi.requests import Request
 
 from app.router import pokemon, health
 from fastapi.middleware.cors import CORSMiddleware
-
 
 app = FastAPI(
     title="Pokemon API",
@@ -23,6 +24,23 @@ app.add_middleware(
 )
 
 API_V1 = "/api/v1"
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index_route(request: Request):
+    html_content = f"""
+    <!doctype html>
+<html>
+  <head>
+    <title>Pokemon API!</title>
+  </head>
+  <body>
+    <h3> For the Swagger API docs got to {request.base_url}docs <br/>thanks for using our service :) </h3>
+  </body>
+</html>
+    """
+    return HTMLResponse(content=html_content, status_code=status.HTTP_200_OK)
+
 
 app.include_router(health.router, prefix=API_V1, tags=["Health"])
 app.include_router(pokemon.router, prefix=API_V1, tags=["Pokemon"])
